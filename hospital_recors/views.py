53e26175350fd3e9baa0e583bb6ab56557dce3f9
Anshuman Nautiyal <anshuman941200@outlook.com>
2021-05-20 23:12:49 +0530

    href

--- hospital_recors/views.py
+++ hospital_recors/views.py
@@ -5,8 +5,10 @@ from . import models
 import json
 # import JsonResponse
 # Create your views here.
-is_auth = False
+is_auth=False
 def open(request):
+	global is_auth
+	print is_auth
 	if 'username' in request.GET:
 		username = request.GET['username']
 		password = request.GET['password']
@@ -14,74 +16,74 @@ def open(request):
 		login = [username,password]
 		is_auth = check_authentication(login)
 		return HttpResponse(json.dumps(is_auth))
+	if is_auth:
+		if 'ship_name' in request.GET:
+			ship_name = request.GET['ship_name']
+			last_service = request.GET['last_service']
+			ship_obj = models.ship_record()
+			ship_obj.name = ship_name
+			ship_obj.last_service = last_service
+			ship_obj.save()
 
-	if 'ship_name' in request.GET:
-		ship_name = request.GET['ship_name']
-		last_service = request.GET['last_service']
-		ship_obj = models.ship_record()
-		ship_obj.name = ship_name
-		ship_obj.last_service = last_service
-		ship_obj.save()
+		if 'ship_id' in request.GET and 'voyage_no' in request.GET:
+			ship_id = request.GET['ship_id']
+			voyage_no = request.GET['voyage_no']
+			make_new_voyage(ship_id,voyage_no)
+		ship_obj = models.ship_record.objects.all()
+		context = dict()
+		context['ship_obj'] = ship_obj
+		# noon_obj = models.noon_report.objects.filter(voyage=7)
+		# print "noon",noon_obj
+		# context['noon_obj'] = noon_obj
+		# for key in noon_obj:
+		# 	print key.voyage.voyage_no,key.speed,key.cons,key.date,key.id
+		if 'noon_id' in request.GET:
+			noon_id = request.GET['noon_id']
+			noon_obj = models.noon_report.objects.filter(id=noon_id).first()
+			speed = noon_obj.speed
+			cons  = noon_obj.cons
+			good_cons = compare()
+			for key in good_cons:
+				if float(key) ==  float(noon_obj.speed):
+					good_cons = good_cons[key]
+			noon_data_dict={
+				'speed'    : noon_obj.speed,
+				'cons'	   : noon_obj.cons,
+				'currents' : noon_obj.currents,
+				'winds'	   : noon_obj.winds,
+				'good_cons':good_cons,
+			}
+			return HttpResponse(json.dumps(noon_data_dict))
+		if 'ship_id_noon' in request.GET:
+			ship_id = request.GET['ship_id_noon']
+			voyage = models.ship_voyage.objects.filter(ship=ship_id).all()
+			voyage_dict = {}
+			for key in voyage:
+				voyage_dict[key.id] = key.voyage_no
+			print voyage_dict,"===dict"
+			return HttpResponse(json.dumps(voyage_dict))
+		if 'noon_date' in request.GET:
+			voyage_id = request.GET['voyage_id_noon']
+			noon_obj = models.noon_report()
+			noon_obj.voyage = models.ship_voyage.objects.filter(id=voyage_id)[0]
+			noon_obj.date = request.GET['noon_date']
+			noon_obj.speed = request.GET['speed']
+			noon_obj.cons = request.GET['cons']
+			noon_obj.currents = request.GET['currents']
+			noon_obj.winds = request.GET['winds']
+			noon_obj.save()
 
-	if 'ship_id' in request.GET and 'voyage_no' in request.GET:
-		ship_id = request.GET['ship_id']
-		voyage_no = request.GET['voyage_no']
-		make_new_voyage(ship_id,voyage_no)
-	ship_obj = models.ship_record.objects.all()
-	context = dict()
-	context['ship_obj'] = ship_obj
-	# noon_obj = models.noon_report.objects.filter(voyage=7)
-	# print "noon",noon_obj
-	# context['noon_obj'] = noon_obj
-	# for key in noon_obj:
-	# 	print key.voyage.voyage_no,key.speed,key.cons,key.date,key.id
-	if 'noon_id' in request.GET:
-		noon_id = request.GET['noon_id']
-		noon_obj = models.noon_report.objects.filter(id=noon_id).first()
-		speed = noon_obj.speed
-		cons  = noon_obj.cons
-		good_cons = compare()
-		for key in good_cons:
-			if float(key) ==  float(noon_obj.speed):
-				good_cons = good_cons[key]
-		noon_data_dict={
-			'speed'    : noon_obj.speed,
-			'cons'	   : noon_obj.cons,
-			'currents' : noon_obj.currents,
-			'winds'	   : noon_obj.winds,
-			'good_cons':good_cons,
-		}
-		return HttpResponse(json.dumps(noon_data_dict))
-	if 'ship_id_noon' in request.GET:
-		ship_id = request.GET['ship_id_noon']
-		voyage = models.ship_voyage.objects.filter(ship=ship_id).all()
-		voyage_dict = {}
-		for key in voyage:
-			voyage_dict[key.id] = key.voyage_no
-		print voyage_dict,"===dict"
-		return HttpResponse(json.dumps(voyage_dict))
-	if 'noon_date' in request.GET:
-		voyage_id = request.GET['voyage_id_noon']
-		noon_obj = models.noon_report()
-		noon_obj.voyage = models.ship_voyage.objects.filter(id=voyage_id)[0]
-		noon_obj.date = request.GET['noon_date']
-		noon_obj.speed = request.GET['speed']
-		noon_obj.cons = request.GET['cons']
-		noon_obj.currents = request.GET['currents']
-		noon_obj.winds = request.GET['winds']
-		noon_obj.save()
-
-	if 'voyage_id_vd' in request.GET:
-		voyage_id = request.GET['voyage_id_vd']
-		noon_obj = models.noon_report.objects.filter(voyage=voyage_id)
-		noon_dict = {}
-		for noons in noon_obj:
-			print noons.id
-			noon_dict[noons.id] = noons.date
-		return HttpResponse(json.dumps(noon_dict))
-	comp_dict = compare()
-	context['comp_dict'] = comp_dict
-	return render_to_response('hospital_recors/home.html',context)
+		if 'voyage_id_vd' in request.GET:
+			voyage_id = request.GET['voyage_id_vd']
+			noon_obj = models.noon_report.objects.filter(voyage=voyage_id)
+			noon_dict = {}
+			for noons in noon_obj:
+				print noons.id
+				noon_dict[noons.id] = noons.date
+			return HttpResponse(json.dumps(noon_dict))
+		comp_dict = compare()
+		context['comp_dict'] = comp_dict
+		return render_to_response('hospital_recors/home.html',context)
 	# return HttpResponse('no')
 def make_new_voyage(ship_id,voyage_no):
 	voyage = models.ship_voyage()
